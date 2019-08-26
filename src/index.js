61a60dc9896d2af0252bfb0046efac3cfd4762ec
Ainur Farkh <farkhutdinov1398@gmail.com>
2019-08-26 20:39:17 +0300

    UserSettings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { StoreContext } from 'redux-react-hook';
 import { Provider } from 'react-redux';
 
 import App from './App';
